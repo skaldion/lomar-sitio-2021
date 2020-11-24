574dfaea8556fb0bbdc695b37e18313093c099f1
diego <diego.orduna@gmail.com>
2020-11-23 22:48:42 -0600

    First pass of the index layout.

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,32 @@
+const fs = require('fs');
+
 module.exports = function (eleventyConfig) {
-	eleventyConfig.setTemplateFormats(['ejs', 'md']);
+	eleventyConfig.setTemplateFormats(['ejs', 'md', 'html']);
 
 	eleventyConfig.setUseGitIgnore(false);
 
 	eleventyConfig.addPassthroughCopy('images/');
 	eleventyConfig.addPassthroughCopy({ 'site/_processed': 'assets/' });
+
+	eleventyConfig.setEjsOptions({
+		rmWhitespace: true,
+	});
+
+	eleventyConfig.setBrowserSyncConfig({
+		callbacks: {
+			ready: function (err, bs) {
+				bs.addMiddleware('*', (req, res) => {
+					const content_404 = fs.readFileSync('dist/404.html');
+					// Provides the 404 content without redirect.
+					res.write(content_404);
+					// Add 404 http status code in request header.
+					// res.writeHead(404, { "Content-Type": "text/html" });
+					res.writeHead(404);
+					res.end();
+				});
+			},
+		},
+	});
 
 	return {
 		dir: {
