861f87be2e23f95ad36c605d0f44c1b1024d1f67
diego <diego.orduna@gmail.com>
2020-12-07 20:02:59 -0600

    Added content to index.ejs.

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addPassthroughCopy('images/');
 	eleventyConfig.addPassthroughCopy({ 'site/_processed': 'assets/' });
+	eleventyConfig.addPassthroughCopy({ 'fonts/': 'assets/' });
 
 	eleventyConfig.setEjsOptions({
 		rmWhitespace: true,
